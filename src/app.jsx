edb35fc9d34a00d6f4045c432dd228a3e4e5c74f
alsry922 <alsry922@gmail.com>
2021-08-20 18:39:36 +0900

    Feature: update navbar count

--- src/app.jsx
+++ src/app.jsx
@@ -5,12 +5,64 @@ import Input from "./components/input";
 import Navbar from "./components/navbar";
 
 class App extends Component {
+  state = {
+    habits: [
+      {
+        id: 1,
+        name: "Reading",
+        count: 0,
+      },
+      {
+        id: 2,
+        name: "Eating",
+        count: 0,
+      },
+      {
+        id: 3,
+        name: "Coding",
+        count: 0,
+      },
+    ],
+  };
+
+  handleIncrement = (habit) => {
+    const habits = [...this.state.habits];
+    const index = habits.indexOf(habit);
+    habits[index].count++;
+    this.setState({ habits });
+  };
+
+  handleDecrement = (habit) => {
+    const habits = [...this.state.habits];
+    const index = habits.indexOf(habit);
+    const count = habits[index].count - 1;
+    habits[index].count = count < 0 ? 0 : count;
+    this.setState({ habits });
+  };
+
+  handleDelete = (habit) => {
+    const habits = [...this.state.habits].filter((item) => {
+      return item.id !== habit.id;
+    });
+    this.setState({ habits });
+  };
+
+  handleReset = () => {
+    const habits = [];
+    this.setState({ habits });
+  };
+
   render() {
     return (
       <>
-        <Navbar></Navbar>
-        <Input></Input>
-        <Habits></Habits>;
+        <Navbar habits={this.state.habits} />
+        <Input />
+        <Habits
+          habits={this.state.habits}
+          onIncrement={this.handleIncrement}
+          onDecrement={this.handleDecrement}
+          onDelete={this.handleDelete}
+        />
       </>
     );
   }
--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -2,11 +2,16 @@ import React, { Component } from "react";
 
 class Navbar extends Component {
   render() {
+    let habitCount = 0;
+    this.props.habits.forEach((habit) => {
+      if (habit.count <= 0) return;
+      habitCount = habitCount + 1;
+    });
     return (
       <nav className="navbar">
         <i className="fas fa-leaf navbar-logo"></i>
         Habit Tracker
-        <span className="navbar-count">0</span>
+        <span className="navbar-count">{habitCount}</span>
       </nav>
     );
   }
